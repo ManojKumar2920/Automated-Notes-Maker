8d4db78c3a8ed2d20e1a2ecbab24a581897e87ef
Manoj Kumar <119614928+ManojKumar2920@users.noreply.github.com>
2023-03-11 15:32:03 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,6 +8,15 @@ st.set_page_config(
     initial_sidebar_state='expanded'
 )
 
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+dir_of_interest = os.path.join(PARENT_DIR, "resources")
+IMAGE_PATH = os.path.join(dir_of_interest,"cover.png")
+
+img = image.imread(IMAGE_PATH)
+
+st.image(img,width=200)
+
 st.title(':red[Automated Notes Maker] :copyright:')
 
 st.write('Automated notes maker from audio recordings is a tool used to covert the recordings of online classes and audio file to document notes . We build this using some Machine Learning algorithms to transcribe the spoken words into text and then create a summary or notes based on the transcribed text')
