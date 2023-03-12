b976f7149fecacea048a24f5cd7cdaf92c3e0ec1
Manoj Kumar <119614928+ManojKumar2920@users.noreply.github.com>
2023-03-13 01:26:33 +0530

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -2,59 +2,8 @@ import streamlit as st
 from matplotlib import image
 import os
 
-st.set_page_config(page_title="Our Team", page_icon=":guardsman:", layout="wide")
+#Mano
 
-# Define CSS style for centering text
-centered_text = """
-    <style>
-        .centered {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            height: 100%;
-        }
-    </style>
-"""
-
-# Add CSS styling to Streamlit app
-st.markdown(centered_text, unsafe_allow_html=True)
-
-# Define CSS style for buttons
-button_style = """
-    <style>
-        .button {
-            border-radius: 20px;
-            border: none;
-            color: white;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            transition-duration: 0.4s;
-            cursor: pointer;
-        }
-
-        .button-blue {
-            background-color: #0072b1;
-        }
-
-        .button-white {
-            background-color: white;
-            color: #0072b1;
-        }
-
-        .button-pink {
-            background-color: #e1306c;
-        }
-    </style>
-"""
-
-# Add CSS styling to Streamlit app
-st.markdown(button_style, unsafe_allow_html=True)
-
-# Mano
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
 dir_of_interest = os.path.join(PARENT_DIR, "resources")
@@ -62,28 +11,28 @@ IMAGE_PATH = os.path.join(dir_of_interest, "Mano.jpg")
 
 img = image.imread(IMAGE_PATH)
 
-col1, col2 = st.columns([1, 4])
-with col1:
-    st.image(img, width=150, caption='Manoj Kumar')
+st.image(img,width=200)
 
-with col2:
-    st.header('Manoj Kumar')
+st.header('Manoj Kumar')
 
-click = st.button('Know about Manoj', key='Manoj')
+click = st.button('Know about Manoj')
 if click == True:
     st.write('Data Science Enthusiast')
     st.write('Follow and Connect with on these platforms !')
     
+
     url = 'https://www.linkedin.com/in/manojkumar20'
-    st.markdown(f'''<a href={url} class="button button-blue">LinkedIn</a>''',unsafe_allow_html=True)
+    st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
     
     url = 'https://github.com/ManojKumar2920'
-    st.markdown(f'''<a href={url} class="button button-white">Github</a>''',unsafe_allow_html=True) 
+    st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
     
     url = 'https://instagram.com/mano._29'
-    st.markdown(f'''<a href={url} class="button button-pink">Instagram</a>''',unsafe_allow_html=True) 
+    st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True) 
+    
 
-# Janani
+#Janani
+
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
 dir_of_interest = os.path.join(PARENT_DIR, "resources")
@@ -91,14 +40,50 @@ IMAGE_PATH = os.path.join(dir_of_interest, "Jana.jpeg")
 
 img = image.imread(IMAGE_PATH)
 
-col1, col2 = st.columns([1, 4])
-with col1:
-    st.image(img, width=150, caption='Janani')
+st.image(img,width=200)
 
-with col2:
-    st.header('Janani')
+st.header('Janani')
 
-click2 = st.button('Know about Janani', key='Janani')
+click2 = st.button('Know about Janani')
 if click2 == True:
     st.write('Full Stack Enthusiast')
     st.write('Follow and Connect with on these platforms !')
+    
+
+    url = 'https://www.linkedin.com/in/janani-p-7a791222a'
+    st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
+    
+    url = 'https://github.com/Jananikani'
+    st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
+    
+    url = 'https://www.instagram.com/janani_kanii/'
+    st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True) 
+    
+
+#Siva
+
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+dir_of_interest = os.path.join(PARENT_DIR, "resources")
+IMAGE_PATH = os.path.join(dir_of_interest, "Siva.jpg")
+
+img = image.imread(IMAGE_PATH)
+
+st.image(img,width=200)
+
+st.header('Siva Subramanian')
+
+click3 = st.button('Know about Siva')
+if click3 == True:
+    st.write('Full Stack Enthusiast')
+    st.write('Follow and Connect with on these platforms !')
+    
+
+    url = 'https://www.linkedin.com/in/siva-subramanian-86017022a'
+    st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
+    
+    url = 'https://github.com/G-Siva'
+    st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
+    
+    url = 'https://www.instagram.com/siva_subramanian_1909'
+    st.markdown(f'''<a href={url}><button style="background-
