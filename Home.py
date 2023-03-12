821266d9a19992ca61df79e4943184c3f073d534
Manoj Kumar <119614928+ManojKumar2920@users.noreply.github.com>
2023-03-13 00:53:11 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,15 +9,36 @@ st.set_page_config(
     initial_sidebar_state='expanded'
 )
 
+# Define CSS styles
+css_styles = """
+<style>
+h1 {
+    color: #FF0000;
+    text-align: center;
+    font-size: 50px;
+    margin-top: 50px;
+}
+h2 {
+    color: #008080;
+    text-align: center;
+    font-size: 30px;
+    margin-top: 30px;
+}
+p {
+    font-size: 20px;
+    text-align: justify;
+    margin: 20px 0;
+}
+</style>
+"""
 
-st.title(':red[Automated Notes Maker] :copyright:')
+# Render CSS styles
+st.markdown(css_styles, unsafe_allow_html=True)
 
-st.write('Automated notes maker from audio recordings is a tool used to covert the recordings of online classes and audio file to document notes . We build this using some Machine Learning algorithms to transcribe the spoken words into text and then create a summary or notes based on the transcribed text')
-
-st.subheader('Authors')
+# Render content with CSS styles applied
+st.title('Automated Notes Maker')
+st.header('Authors')
 st.write('Manoj Kumar')
 st.write('Janani')
 st.write('Siva Subramanian')
-
-    
-    
+st.write('Automated notes maker from audio recordings is a tool used to covert the recordings of online classes and audio file to document notes. We build this using some Machine Learning algorithms to transcribe the spoken words into text and then create a summary or notes based on the transcribed text.')
