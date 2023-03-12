213fff1ccafeaaa779443a25984aa8e49a09a82f
Manoj Kumar <119614928+ManojKumar2920@users.noreply.github.com>
2023-03-13 01:30:54 +0530

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -2,7 +2,7 @@ import streamlit as st
 from matplotlib import image
 import os
 
-#Mano
+# Mano
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
@@ -11,27 +11,31 @@ IMAGE_PATH = os.path.join(dir_of_interest, "Mano.jpg")
 
 img = image.imread(IMAGE_PATH)
 
-st.image(img,width=200)
+# First column: Image
+col1, col2 = st.columns([1, 3])
+with col1:
+    st.image(img, width=200)
 
-st.header('Manoj Kumar')
+# Second column: Information
+with col2:
+    st.header('Manoj Kumar')
 
-click = st.button('Know about Manoj')
-if click == True:
-    st.write('Data Science Enthusiast')
-    st.write('Follow and Connect with on these platforms !')
-    
+    click = st.button('Know about Manoj')
+    if click == True:
+        st.write('Data Science Enthusiast')
+        st.write('Follow and Connect with on these platforms !')
 
-    url = 'https://www.linkedin.com/in/manojkumar20'
-    st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
-    
-    url = 'https://github.com/ManojKumar2920'
-    st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
-    
-    url = 'https://instagram.com/mano._29'
-    st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True) 
-    
+        url = 'https://www.linkedin.com/in/manojkumar20'
+        st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
 
-#Janani
+        url = 'https://github.com/ManojKumar2920'
+        st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True)
+
+        url = 'https://instagram.com/mano._29'
+        st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True)
+
+
+# Janani
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
@@ -40,27 +44,31 @@ IMAGE_PATH = os.path.join(dir_of_interest, "Jana.jpeg")
 
 img = image.imread(IMAGE_PATH)
 
-st.image(img,width=200)
+# First column: Image
+col1, col2 = st.columns([1, 3])
+with col1:
+    st.image(img, width=200)
 
-st.header('Janani')
+# Second column: Information
+with col2:
+    st.header('Janani')
 
-click2 = st.button('Know about Janani')
-if click2 == True:
-    st.write('Full Stack Enthusiast')
-    st.write('Follow and Connect with on these platforms !')
-    
+    click2 = st.button('Know about Janani')
+    if click2 == True:
+        st.write('Full Stack Enthusiast')
+        st.write('Follow and Connect with on these platforms !')
 
-    url = 'https://www.linkedin.com/in/janani-p-7a791222a'
-    st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
-    
-    url = 'https://github.com/Jananikani'
-    st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
-    
-    url = 'https://www.instagram.com/janani_kanii/'
-    st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True) 
-    
+        url = 'https://www.linkedin.com/in/janani-p-7a791222a'
+        st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
 
-#Siva
+        url = 'https://github.com/Jananikani'
+        st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True)
+
+        url = 'https://www.instagram.com/janani_kanii/'
+        st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True)
+
+
+# Siva
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
@@ -69,21 +77,25 @@ IMAGE_PATH = os.path.join(dir_of_interest, "Siva.jpg")
 
 img = image.imread(IMAGE_PATH)
 
-st.image(img,width=200)
+# First column: Image
+col1, col2 = st.columns([1, 3])
+with col1:
+    st.image(img, width=200)
 
-st.header('Siva Subramanian')
+# Second column: Information
+with col2:
+    st.header('Janani')
 
-click3 = st.button('Know about Siva')
-if click3 == True:
-    st.write('Full Stack Enthusiast')
-    st.write('Follow and Connect with on these platforms !')
-    
+    click2 = st.button('Know about Janani')
+    if click2 == True:
+        st.write('Full Stack Enthusiast')
+        st.write('Follow and Connect with on these platforms !')
 
-    url = 'https://www.linkedin.com/in/siva-subramanian-86017022a'
-    st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
-    
-    url = 'https://github.com/G-Siva'
-    st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
-    
-    url = 'https://www.instagram.com/siva_subramanian_1909'
-    st.markdown(f'''<a href={url}><button style="background-
+        url = 'https://www.linkedin.com/in/siva-subramanian-86017022a'
+        st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
+
+        url = 'https://github.com/G-Siva'
+        st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True)
+
+        url = 'https://www.instagram.com/siva_subramanian_1909/'
+        st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True)
