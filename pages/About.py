54951f1375d2c9051def64e397e92c2db94a7ebb
Manoj Kumar <119614928+ManojKumar2920@users.noreply.github.com>
2023-03-13 01:40:46 +0530

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -11,26 +11,25 @@ IMAGE_PATH = os.path.join(dir_of_interest, "Mano.jpg")
 
 img = image.imread(IMAGE_PATH)
 
-st.image(img, width=200, use_column_width=False, align="left")
+st.image(img,width=200)
 
-st.header('Manoj Kumar', style="text-align:center")
+st.header('Manoj Kumar')
 
-col1, col2 = st.beta_columns([2, 1])
+click = st.button('Know about Manoj')
+if click == True:
+    st.write('Data Science Enthusiast')
+    st.write('Follow and Connect with on these platforms !')
+    
 
-with col1:
-    click = st.button('Know about Manoj')
-    if click == True:
-        st.write('Data Science Enthusiast')
-        st.write('Follow and Connect with on these platforms !')
-        url = 'https://www.linkedin.com/in/manojkumar20'
-        st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
-        url = 'https://github.com/ManojKumar2920'
-        st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
-        url = 'https://instagram.com/mano._29'
-        st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True) 
-
-with col2:
-    st.write("")  # empty column to adjust layout
+    url = 'https://www.linkedin.com/in/manojkumar20'
+    st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
+    
+    url = 'https://github.com/ManojKumar2920'
+    st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
+    
+    url = 'https://instagram.com/mano._29'
+    st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True) 
+    
 
 #Janani
 
@@ -41,26 +40,25 @@ IMAGE_PATH = os.path.join(dir_of_interest, "Jana.jpeg")
 
 img = image.imread(IMAGE_PATH)
 
-st.image(img, width=200, use_column_width=False, align="left")
+st.image(img,width=200)
 
-st.header('Janani', style="text-align:center")
+st.header('Janani')
 
-col1, col2 = st.beta_columns([2, 1])
+click2 = st.button('Know about Janani')
+if click2 == True:
+    st.write('Full Stack Enthusiast')
+    st.write('Follow and Connect with on these platforms !')
+    
 
-with col1:
-    click2 = st.button('Know about Janani')
-    if click2 == True:
-        st.write('Full Stack Enthusiast')
-        st.write('Follow and Connect with on these platforms !')
-        url = 'https://www.linkedin.com/in/janani-p-7a791222a'
-        st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
-        url = 'https://github.com/Jananikani'
-        st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
-        url = 'https://www.instagram.com/janani_kanii/'
-        st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True) 
-
-with col2:
-    st.write("")  # empty column to adjust layout
+    url = 'https://www.linkedin.com/in/janani-p-7a791222a'
+    st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
+    
+    url = 'https://github.com/Jananikani'
+    st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
+    
+    url = 'https://www.instagram.com/janani_kanii/'
+    st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True) 
+    
 
 #Siva
 
@@ -71,23 +69,21 @@ IMAGE_PATH = os.path.join(dir_of_interest, "Siva.jpg")
 
 img = image.imread(IMAGE_PATH)
 
-st.image(img, width=200, use_column_width=False, align="left")
+st.image(img,width=200)
 
-st.header('Siva Subramanian', style="text-align:center")
+st.header('Siva Subramanian')
 
-col1, col2 = st.beta_columns([2, 1])
+click3 = st.button('Know about Siva')
+if click3 == True:
+    st.write('Full Stack Enthusiast')
+    st.write('Follow and Connect with on these platforms !')
+    
 
-with col1:
-    click2 = st.button('Know about Siva')
-    if click2 == True:
-        st.write('Full Stack Enthusiast')
-        st.write('Follow and Connect with on these platforms !')
-        url = 'https://www.linkedin.com/in/janani-p-7a791222a'
-        st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
-        url = 'https://github.com/G-Siva'
-        st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
-        url = 'https://www.instagram.com/siva_subramanian_1909/'
-        st.markdown(f'''<a href={url}><button style="background-color:lightpink;">Instagram</button></a>''',unsafe_allow_html=True) 
-
-with col2:
-    st.write("")  # empty column to adjust layout
+    url = 'https://www.linkedin.com/in/siva-subramanian-86017022a'
+    st.markdown(f'''<a href={url}><button style="background-color:lightblue;">Linkedin</button></a>''',unsafe_allow_html=True)
+    
+    url = 'https://github.com/G-Siva'
+    st.markdown(f'''<a href={url}><button style="background-color:white;">Github</button></a>''',unsafe_allow_html=True) 
+    
+    url = 'https://www.instagram.com/siva_subramanian_1909'
+    st.markdown(f'''<a href={url}><button style="background-
